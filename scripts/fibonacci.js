2b2a5ebd3a2bb9cc5e2edb0f76b0ea6562c5c4ab
Nico <nicodiazan@gmail.com>
2023-07-19 15:52:27 +0200

    Add non-recursive solution to fibonacci

--- scripts/fibonacci.js
+++ scripts/fibonacci.js
@@ -2,7 +2,7 @@ import { setupInput } from "./input";
 
 function setupFibonacci(element) {
 	const wrapper = document.createElement("div");
-    wrapper.className = "calcWrapper"
+	wrapper.className = "calcWrapper";
 
 	wrapper.appendChild(
 		setupInput(calculateFib, "Non-recursive Fibonacci", "normalFib")
@@ -15,10 +15,21 @@ function setupFibonacci(element) {
 }
 
 function calculateFib(n) {
-	return n + n;
+	n = Number(n);
+	const fibArr = [];
+	for (let i = 0; i < n; i++) {
+		const len = fibArr.length;
+		if (len < 2) {
+			fibArr.push(1);
+			continue;
+		}
+		fibArr.push(Number(fibArr[len-1]) + Number(fibArr[len - 2]));
+	}
+	return fibArr;
 }
 
 function recursiveFib(n) {
+	n = Number(n);
 	return n + n;
 }
 
