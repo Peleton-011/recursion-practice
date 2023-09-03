5c0c45392a72bbe314e481a4e43bdb67380a5eb8
Nico <nicodiazan@gmail.com>
2023-09-03 20:46:19 +0200

    Set up some classes to facilitate finding the smallest multiple

--- scripts/euler/euler5.js
+++ scripts/euler/euler5.js
@@ -9,6 +9,45 @@ function setupEuler5() {
 	return wrapper;
 }
 
+function smallestMultipleUnder(n) {
+	const factors = new FactorList();
+
+	const primes = primeNumbersUnder(n);
+	primes.forEach((p) => factors.addFactor(p, 1));
+
+	for (let i = 2; i < Number(n) + 1; i++) {
+        if (primes.indexOf(i) >= 0) {
+            continue;
+		}
+        
+	}
+}
+
+class FactorList {
+	list = [];
+
+	addFactor(prime, exponent) {
+		this.list.push(new Factor(prime, exponent));
+	}
+
+	product() {
+		return this.list.reduce(
+			(acc, factor) => acc * factor.num ** factor.exp,
+			1
+		);
+	}
+}
+
+class Factor {
+	constructor(prime, exponent) {
+		this.num = prime;
+		this.exp = exponent || 1;
+	}
+
+	setExponent(n) {
+		this.exp = n;
+	}
+}
 
 function primeNumbersUnder(n) {
 	const primeList = [2];
@@ -33,6 +72,5 @@ function getNextPrime(curr) {
 
 	return last;
 }
-
 
 export { setupEuler5 };
