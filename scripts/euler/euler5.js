0e8efe3172e8703af622fd816b750cc95ef6c4d0
Nico <nicodiazan@gmail.com>
2023-09-03 19:12:08 +0200

    Add some boilerplate for calculating primes

--- scripts/euler/euler5.js
+++ scripts/euler/euler5.js
@@ -4,39 +4,35 @@ function setupEuler5() {
 	const wrapper = document.createElement("div");
 	wrapper.className = "calcWrapper";
 
-	wrapper.appendChild(setupInput(largestPrimeFactor, "Euler 5", "euler5"));
+	wrapper.appendChild(setupInput(smallestMultipleUnder, "Euler 5", "euler5"));
 
 	return wrapper;
 }
 
-function largestPrimeFactor(n) {
-	const primes = [];
-	const primeFactors = [];
 
-	while (primes[primes.length - 1] < n || primes == 0) {
-		primes.push(getNextPrime(primes));
-
-		if (n % primes[primes.length - 1] === 0) {
-			n /= primes[primes.length - 1];
-
-			primeFactors.push(primes[primes.length - 1]);
-		}
+function primeNumbersUnder(n) {
+	const primeList = [2];
+	console.log(n);
+	while (primeList[primeList.length - 1] <= n) {
+		// console.log(primeList[primeList.length - 1])
+		primeList.push(getNextPrime(primeList));
 	}
-
-	return primeFactors[primeFactors.length - 1];
+	primeList.pop();
+	return primeList;
 }
 
 function getNextPrime(curr) {
-    if (curr.length < 1) return 2
+	if (curr.length < 1) return 2;
 	//TODO improve this function
 
 	let last = curr[curr.length - 1];
 
-    while (curr.some((prime) => last % prime === 0)) {
-        last++;
-    }
+	while (curr.some((prime) => last % prime === 0)) {
+		last++;
+	}
 
-    return last
+	return last;
 }
+
 
 export { setupEuler5 };
